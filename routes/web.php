a9a1edbb17fcc43cf68f6c51c7e9ee449a51d489
dumondmd <dmd.dumon@gmail.com>
2020-04-09 17:21:54 -0300

    Definindo as rotas do CRUD

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,12 @@ Route::get('/contato/{id?}', ['uses'=>'ContatoController@index']);
 Route::post('/contato', ['uses'=>'ContatoController@criar']);
 
 Route::put('/contato', ['uses'=>'ContatoController@editar']);
+
+
+Route::get('/admin/cursos', 'Admin\CursosController@index')->name('admin.cursos');
+Route::get('/admin/cursos/adicionar', 'Admin\CursosController@adicionar')->name('admin.cursos.adicionar');
+Route::post('/admin/cursos/salvar', 'Admin\CursosController@salvar')->name('admin.cursos.salvar');
+Route::get('/admin/cursos/editar/{id}', 'Admin\CursosController@editar')->name('admin.cursos.editar');
+Route::put('/admin/cursos/atualizar/{id}', 'Admin\CursosController@atualizar')->name('admin.cursos.atualizar');
+Route::get('/admin/cursos/deletar/{id}', 'Admin\CursosController@deletar')->name('admin.cursos.deletar');
 	
