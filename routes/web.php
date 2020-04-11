28e61a2896a0f857c6701acba8a67ee1b57cb098
dumondmd <dmd.dumon@gmail.com>
2020-04-10 21:47:43 -0300

    Listando cursos na home

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Site\HomeController@index')->name('site.home');
 
 Route::get('/contato/{id?}', ['uses'=>'ContatoController@index']);
 
